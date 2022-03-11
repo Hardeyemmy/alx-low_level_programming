dc97cd1f8842a86ff644d15fb116112947dd2304
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-11 03:45:14 -0800

    0 positive or- negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,8 @@
  *
  */
 int main(void)
- {
- 	int n;
+{
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -28,4 +28,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
- }	
+}	
