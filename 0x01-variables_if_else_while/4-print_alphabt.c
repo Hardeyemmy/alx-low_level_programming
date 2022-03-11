10123ea11bf15807c9bb11675ba6f97f4079867e
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-11 10:01:07 -0800

    4 print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print a function
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q)
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
