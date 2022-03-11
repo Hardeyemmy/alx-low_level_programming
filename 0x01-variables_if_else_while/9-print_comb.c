867b4b61fa2bc64e2cce2a003ede4ff556fd2f81
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-11 14:38:35 -0800

    9 print comb c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print all positive single digits
+ *
+ * Return: End of program
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x =< '9')
+	{
+		putchar(x);
+		if (x != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
