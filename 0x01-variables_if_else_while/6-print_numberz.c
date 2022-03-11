8855b316a8a29d5a7d957cfa07a9ba92e7900440
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-11 10:48:41 -0800

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print all numbers between 0 and 9 with putchar
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+	int s;
+
+	for (s = '0'; s <= '9'; s++)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+	result (0);
+}
