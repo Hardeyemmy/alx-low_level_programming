939d4e8ecf8e0afd159d81f8cb7ae356d4f2f593
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-12 01:21:39 -0800

    5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Use Printf
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
