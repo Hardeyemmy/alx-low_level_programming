691349b444fde1f71fd698ea5265479d0c7b4245
Hardeyemmy <50312221+Hardeyemmy@users.noreply.github.com>
2022-03-15 19:36:43 +0100

    2 alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+include "main.h"
+/**
+ * main - Print alphabet 10x
+ *
+ * Description: The body of the program
+ *
+ * Return: End the program
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int n = 10;
+
+	while (n >= 1)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			++a;
+		}
+		--n;
+		_putchar(10);
+		a = 'a';
+	}
+}
