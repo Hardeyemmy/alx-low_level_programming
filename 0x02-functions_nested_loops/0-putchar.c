6387059d6fcdcca9a8ddabde903906d2b8fc0ac4
Hardeyemmy <50312221+Hardeyemmy@users.noreply.github.com>
2022-03-15 17:12:21 +0100

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,14 @@
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	char string[8] = "_putchar";
+	int n = 0;
+
+	while (n < 8)
+	{
+	_putchar(string[n]);
+	++n;
+	}
+	_putchar(10);
 	return (0);
 }
