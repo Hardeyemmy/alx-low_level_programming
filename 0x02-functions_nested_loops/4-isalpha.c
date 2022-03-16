2b6562f793046f52435713a1aefeba5529383dd7
Hardeyemmy <50312221+Hardeyemmy@users.noreply.github.com>
2022-03-16 13:20:47 +0100

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check for uppercase letters
+ *
+ * Return: 1 if c is uppercase  or 0 if c is anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c = 'z'))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
