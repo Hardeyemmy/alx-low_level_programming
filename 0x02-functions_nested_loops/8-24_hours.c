a21aedcf3e64e6a52d1a4b2e4cdcfa3dcf68d983
Hardeyemmy <50312221+Hardeyemmy@users.noreply.github.com>
2022-03-16 19:31:57 +0100

    8 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - jack bauer countdown
+ *
+ * Return: End the program
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+	for (minute = 0; minute <= 59; minute++)
+	{
+	_putchar('0' + (hour / 10));
+	_putchar('0' + (hour % 10));
+	_putchar(':');
+	_putchar('0' + (minute / 10));
+	_putchar('0' + (minute % 10));
+	_putchare('\n');
+	}
+	}
+}
