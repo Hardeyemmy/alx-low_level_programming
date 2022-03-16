3339900ca3bbe63978f27daacd7188fa6e197db1
Hardeyemmy <50312221+Hardeyemmy@users.noreply.github.com>
2022-03-16 04:55:32 +0100

    3 islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check if the lower case is true
+ *
+ * Description: it returns 1 if its lower and 0 if not
+ *
+ * Return: always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
