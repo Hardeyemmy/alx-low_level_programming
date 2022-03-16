d6f20930d23e2f0ec071931d69d58a650f1090b8
Hardeyemmy <50312221+Hardeyemmy@users.noreply.github.com>
2022-03-16 05:15:50 +0100

    3 is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * main - check if the lower case is true
- *
- * Description: it returns 1 if its lower and 0 if not
- *
+ *Description: it returns 1 if its lower and 0 if not
  * Return: always 0
  */
 int _islower(int c)
