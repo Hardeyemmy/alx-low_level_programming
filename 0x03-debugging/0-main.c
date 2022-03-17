7ffff374518a3932f717751993b196118d9bfd56
Hardeyemmy <50312221+Hardeyemmy@users.noreply.github.com>
2022-03-17 07:11:06 +0100

    adding main.h, 0-main.c and readme file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test a function that print if the integer is positive or negative
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
