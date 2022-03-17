37287aa98051be8bb8e4684851641d2b14ddb28d
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-17 13:42:32 -0700

    6 print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,9 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
+	while (n-- > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-	for (i = 1; i <= n; i++)
-	{
-		_putchar('-');
+	_putchar('-');
 	}
 	_putchar('\n');
-	}
 }
