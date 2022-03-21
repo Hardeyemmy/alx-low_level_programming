54abd20a7c8cfc8584877ca32e411206103094b7
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-21 01:51:10 -0700

    0 reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * reset_to_98 - print an int and update it 
- *
+ * reset_to_98 - print an int and update it
+ * 
  * Return: End the program
  */
 void reset_to_98(int *n)
 {
+	int n;
+
 	*n = 98;
 
 }
