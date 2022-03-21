cd62ffea24ea861411656644e99311c23b37357d
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-21 09:35:30 -0700

    9 strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include " main.h"
+/**
+ * *_strcpy - function that copy a string to the source src
+ *
+ * @dest: pointer parameter 1
+ * @src: pointer parameter 2
+ *
+ * Return: End the program
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	}
+	while (dest[i]  != '\0')
+		return (dest);
+}
