cda0b2fdbba3a87f5dfbc578ccbe47748365c341
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-24 09:55:13 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - function that changes all lowercase of string to uppercase
+ *
+ * @s: pointer parameter 
+ *
+ * Return: return *s
+ */
+char *string_troupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] = '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
