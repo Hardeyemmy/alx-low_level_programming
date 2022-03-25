52ac4b7543ff3ecb9f56fdcce7740ea4e322a14d
Hardeyemmy <iiadeyemi307@gmail.com>
2022-03-25 12:47:25 -0700

    100 rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string using rot13
+ *
+ * @s: pointer parameter pass into the function
+ *
+ * Return: End the program
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	int data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; i != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+		if (s[i] == data1[j])
+		{
+			s[i] = datarot[j];
+			break;
+		}
+		}
+	}
+	return (s);
+}
