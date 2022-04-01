946f7ca6f42dca10bb963e3c835c455ee78369ba
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-01 03:49:16 -0700

    2 args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function that prints all the arguement it receives
+ *
+ * @argc: offset count
+ * @argv: offset value
+ *
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argc[i]);
+	return (0);
+	}
+}
