b3e67022316d2b8d0cf081a69e9c64e3da9d2c72
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-01 03:39:04 -0700

    1 args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function that print a number of arguement passed into it
+ *
+ * @argc: n arg
+ * @argv: args array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[]__attribute__(unused))
+{
+	if (argc > 0)
+	{
+	printf("%d\n", argc - 1);
+	return (0);
+	}
+}
