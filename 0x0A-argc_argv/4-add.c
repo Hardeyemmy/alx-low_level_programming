198b81cfae47c4c13eaa1af750f2b6a73a4f5a46
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-01 09:05:35 -0700

    4 add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * main - function that add five positive numbers
+ *
+ * @argc: n args
+ * @argv: args array
+ *
+ * Return: sum
+ */
+int main(int argc, char *agrv[])
+{
+	unsigned int i, sum, num;
+
+	sum = 0;
+
+	if (agrc < 3)
+	{
+	printf("%d\n", 0);
+	return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (sum);
+}
