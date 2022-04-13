eaa3cf8d9d5d12db4431f9dceeeffe6f2d385f47
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-13 16:23:25 -0700

    modifying 3-op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,7 +10,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t os[] = {
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -24,7 +24,7 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 		{
-			return (ops.[i].f);
+			return (ops[i].f);
 		}
 		i++;
 	}
