0a0f02482b9098bfde4f3d441849ee35d6d95983
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-14 08:52:46 -0700

    modifying 0sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list params;
 
 	va_start(params, n);
-	if (n == 0)
-	{
-		return (0);
-	}
-	else if (n != 0)
+	if (n != 0)
 	{
 		for (i = 0; i < n; i++)
 		{
