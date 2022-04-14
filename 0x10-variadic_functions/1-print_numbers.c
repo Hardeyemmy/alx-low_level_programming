05ddd21544729490755da0d0965933f5625b79b6
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-14 09:59:15 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(argument1, n);
 	if (n != 0)
 	{
-		for (i = 0; i < (n - 1); i++)
+		for (i = 0; (i < n - 1); i++)
 		{
 			if (separator != NULL)
 			{
