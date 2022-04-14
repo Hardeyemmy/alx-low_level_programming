454b06fed71f7159b084befef5ff544223e8a5d0
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-14 08:48:42 -0700

    renaming the root folder

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_function.h"
+/**
+ * sum_them_all - function that return the sum of all its parameter
+ *
+ * @n: num of args passed
+ *
+ * Return: sum of the parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list params;
+
+	va_start(params, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(params, n);
+		}
+	}
+	va_end(params);
+	return (sum);
+}
