2d9e84ebbc0fa7e6a04db04bf6c14544775b5c52
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-21 09:35:08 -0700

    0 print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,10 +11,11 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-			printf("[0] nil\n");
+			printf("[%d] %s\n", h->len, h->str);
+
 		count ++;
 		h = h->next;
 	}
