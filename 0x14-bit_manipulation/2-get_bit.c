730bcc3cea8b65eda628983fd7256e27cac4db6d
Hardeyemmy <iiadeyemi307@gmail.com>
2022-04-28 07:25:17 -0700

    2 get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+	int bit;
 
-	if ((n & (1 << index)) == 0)
-		return (0);
-
-	return (1);
+	bit = (n >> index);
+	if (index > 32)
+	return (-1);
+	return (bit & 1);
 }
