3cb136767a88137ac2b12796285f8eebae79c222
Hardeyemmy <iiadeyemi307@gmail.com>
2022-10-06 11:52:15 -0700

    search algo update

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -11,7 +11,7 @@
  *         Otherwise, a pointer to the first node where the value is located.
  *
  * Description: Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
+ *              Uses the square root of the list size as the jump step
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
